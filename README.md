d8f41d74aa4a8fffc8e26e3204842165e2ba0134
sebastianbk98 <sebastianbk007@gmail.com>
2024-08-05 18:18:57 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TypeScript MERN E-Commerce Project
+
+E-Commerce project with MongoDB ExpressJS and React Typescript
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <header>TypeScript E-Commerce</header>
+      <main></main>
+      <footer>All right reserved</footer>
+    </div>
+  );
+};
+
+export default App;
