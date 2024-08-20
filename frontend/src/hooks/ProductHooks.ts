93be135546efec8fe8c95844c2823d232eb350b9
sebastianbk98 <sebastianbk007@gmail.com>
2024-08-20 22:47:47 +0700

    create CRUD request method to backend

--- frontend/src/hooks/ProductHooks.ts
+++ frontend/src/hooks/ProductHooks.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { Product } from "../types/Product";
 import apiClient from "../API";
 
@@ -13,4 +13,38 @@ export const useGetProductDetailsBySlugQuery = (slug: string) =>
     queryKey: ["products", slug],
     queryFn: async () =>
       (await apiClient.get<Product>(`products/${slug}`)).data,
+  });
+
+export const useAddProductMutation = () =>
+  useMutation({
+    mutationFn: async (formData: FormData) =>
+      (
+        await apiClient.post<{ message: string; product: Product }>(
+          "products/admin",
+          formData,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        )
+      ).data,
+  });
+
+export const useEditProductMutation = () =>
+  useMutation({
+    mutationFn: async (product: Product) =>
+      (
+        await apiClient.put<{ message: string; product: Product }>(
+          `products/admin/${product._id}`,
+          product
+        )
+      ).data,
+  });
+
+export const useDeleteProductMutation = () =>
+  useMutation({
+    mutationFn: async (id: string) =>
+      (await apiClient.delete<{ message: string }>(`products/admin/${id}`))
+        .data,
   });
