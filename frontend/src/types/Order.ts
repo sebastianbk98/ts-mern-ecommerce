dd173425b7de893fedabc17b163814a77c67681b
sebastianbk98 <sebastianbk007@gmail.com>
2025-02-04 12:41:14 +0700

    bug fixes

--- frontend/src/types/Order.ts
+++ frontend/src/types/Order.ts
@@ -1,11 +1,12 @@
 import { CartItem, ShippingAddress } from "./Cart";
+import { User } from "./User";
 
 export type Order = {
   _id: string;
   orderItems: CartItem[];
   shippingAddress: ShippingAddress;
   paymentMethod: string;
-  user: string;
+  user: User;
   createdAt: string;
   isPaid: boolean;
   paidAt: string;
